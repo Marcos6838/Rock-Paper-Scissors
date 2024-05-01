802552bf7961d5c383fa8bcb58ca83a39e0f1766
Marcos <marcos510hernandez@gmail.com>
2024-05-01 00:00:32 -0700

    finished the function that gets the choice of the computer

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+function getComputerChoice(){
+    let num1 = Math.random();
+    return num1;
+}
+
+function getHumanChoice(){
+    
+}
+let cpuChoice = getComputerChoice()
+
+if (cpuChoice >= 0 && cpuChoice < .33){
+    cpuChoice = "rock";
+} else if (cpuChoice >= .33 && cpuChoice <= .66){
+    cpuChoice = "paper";
+} else if (cpuChoice > .66 && cpuChoice <= 1){
+    cpuChoice = "scissor";
+}
+
+console.log(cpuChoice)
