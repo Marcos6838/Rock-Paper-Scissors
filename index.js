b0cdb7e2335f8ee067723e3945a0b3fc78f3abee
Marcos <marcos510hernandez@gmail.com>
2024-05-12 00:43:45 -0700

    figured out bug where winner variable as not updating..... the reason was i was using double == to assignt instead of single =...

--- index.js
+++ index.js
@@ -13,52 +13,63 @@ function getComputerChoice(){
 function getHumanChoice(){
     let human = prompt("Enter Rock,Paper,Scissor").toLowerCase();
     while(human != 'rock' && human != 'paper' && human != 'scissor'){
-        human = prompt("Enter Rock,Paper,Scissor").toLowerCase();
+        human = prompt("Enter Rock,Paper,Scissor only please").toLowerCase();
     }
     return human;
 }
 
-function playRound(humanChoice,ComputerChoice){
+function playRound(humanChoice,ComputerChoice,winner){
+
     if(humanChoice == 'rock' && ComputerChoice == 'paper'){
-        winner == 1;
+        winner = 1;
     } else if(humanChoice == 'rock' && ComputerChoice == 'rock'){
-        winner == 2;
+        winner = 2;
     } else if(humanChoice == 'rock' && ComputerChoice == 'scissor'){
-        winner == 3;
+        winner = 3;
     }
 
     if(humanChoice == 'paper' && ComputerChoice == 'paper'){
-        winner == 2;
+        winner = 2;
     } else if(humanChoice == 'paper' && ComputerChoice == 'rock'){
-        winner == 3;
+        winner = 3;
     } else if(humanChoice == 'paper' && ComputerChoice == 'scissor'){
-        winner == 1;
+        winner = 1;
     }
 
     if(humanChoice == 'scissor' && ComputerChoice == 'paper'){
-        winner == 3;
+        winner = 3;
     } else if(humanChoice == 'scissor' && ComputerChoice == 'rock'){
-        winner == 1;
+        winner = 1;
     } else if(humanChoice == 'scissor' && ComputerChoice == 'scissor'){
-        winner == 2;
+        winner = 2;
     }
     
-    
+    console.log(winner)
     return winner;
 }
 
-
-let cpuChoice = getComputerChoice()
-let humanChoice = getHumanChoice()
-let winner = playRound(cpuChoice,humanChoice);
 let humanScore = 0;
 let computerScore = 0;
 let i = 0;
-while (i <= 5){
+let winner = 0;
+while (i < 5){
+    i+=1;
     let cpuChoice = getComputerChoice()
     let humanChoice = getHumanChoice()
-    let winner = playRound(cpuChoice,humanChoice);
-    i++;
+    winner = playRound(humanChoice,cpuChoice,winner);
+    console.log('Computer choice : ',cpuChoice);
+    console.log('Player choice: ',humanChoice);
+    if(winner == 3){
+        console.log('Player Wins!');
+        humanScore += 1;
+    } else if(winner == 2){
+        console.log('No one wins TIE');
+    } else if(winner == 1){
+        console.log('Computer Wins')
+        computerScore += 1;
+    }
+    
 }
-console.log(cpuChoice);
-console.log(human);
+
+console.log('Player score: ', humanScore);
+console.log('Computer score: ', computerScore);
